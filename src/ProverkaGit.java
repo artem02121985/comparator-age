044424d2a498ded4e48c13f7e8f38ecf5c7b68f6
Artem Kirienko <kiartemchik1985@gmail.com>
2022-09-05 23:29:10 +0300

    2

--- src/ProverkaGit.java
+++ src/ProverkaGit.java
@@ -1,7 +1,7 @@
 public class ProverkaGit {
     public static void main(String[] args) {
 
-        System.out.println("Les");
+        System.out.println("Le");
         System.out.println("Vot");
 
     }
