3618034f3de47f4c75bf566b5c564bbeead0a7c5
artem02121985 <95944672+artem02121985@users.noreply.github.com>
2022-09-05 22:53:21 +0300

    Update ProverkaGit.java
    yyy

--- src/ProverkaGit.java
+++ src/ProverkaGit.java
@@ -1,7 +1,7 @@
 public class ProverkaGit {
     public static void main(String[] args) {
         System.out.println("Lesson");
-        System.out.println("Vot");
+        System.out.println("V");
 
     }
 }
