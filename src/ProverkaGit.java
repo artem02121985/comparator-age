1d4c965bcac204895b0d5f8dfc6f312b01d7d667
artem02121985 <95944672+artem02121985@users.noreply.github.com>
2022-09-07 22:47:58 +0300

    Llll

--- src/ProverkaGit.java
+++ src/ProverkaGit.java
@@ -1,7 +1,7 @@
 public class ProverkaGit {
     public static void main(String[] args) {
 
-        System.out.println("Le");
+        System.out.println("Lllle");
         System.out.println("Vot");
         System.out.println();
         int x = 10;
